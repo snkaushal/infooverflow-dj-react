fa7e0c1e2cd27d75345ce914865fcb2b74c941fb
Sahil <skaushal@breadware.com>
2019-06-24 15:03:54 +0530

    added create, update, delete view for articles

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ArticlesListView,ArticlesDetialView
+from .views import ArticlesListView, ArticlesDetialView, ArticlesCreateView, ArticlesUpdateView, ArticlesDestroyView
 
 urlpatterns = [
     path('', ArticlesListView.as_view(), name='articleslistview'),
-    path('<pk>', ArticlesDetialView.as_view(), name='articlesdetailview')
+    path('create', ArticlesCreateView.as_view(), name='articleslistview'),
+    path('<pk>', ArticlesDetialView.as_view(), name='articlesdetailview'),
+    path('<pk>/update', ArticlesUpdateView.as_view(), name='articlesdetailview'),
+    path('<pk>/delete', ArticlesDestroyView.as_view(), name='articlesdetailview')
 ]
--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
 from articles.models import Articles
 from .serializers import ArticleSerializer
@@ -7,6 +7,18 @@ class ArticlesListView(ListAPIView):
   queryset = Articles.objects.all()
   serializer_class = ArticleSerializer
 
+class ArticlesCreateView(CreateAPIView):
+  queryset = Articles.objects.all()
+  serializer_class = ArticleSerializer
+
 class ArticlesDetialView(RetrieveAPIView):
+  queryset = Articles.objects.all()
+  serializer_class = ArticleSerializer
+
+class ArticlesUpdateView(UpdateAPIView):
+  queryset = Articles.objects.all()
+  serializer_class = ArticleSerializer
+
+class ArticlesDestroyView(DestroyAPIView):
   queryset = Articles.objects.all()
   serializer_class = ArticleSerializer
